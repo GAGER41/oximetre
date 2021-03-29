fad70076e52bb79687968acfa808db95da6ce444
GAGER41 <57801267+GAGER41@users.noreply.github.com>
2021-03-29 13:53:16 -0400

    nouvelles fig

--- figure.py
+++ figure.py
@@ -15,20 +15,22 @@ pouls_e = data[4]
 # figure taux
 
 f = plt.figure()
-plt.plot(essais, taux_c, "bo", label="oximètre commercial")
-plt.plot(essais, taux_e, "rx", label="résultats expérimentaux")
+plt.plot(essais, taux_c, "ko", label="oximètre commercial")
+plt.plot(essais[0], taux_e[0], "kx", label="résultats expérimentaux")
 plt.legend()
-#plt.errorbar(essais, taux_e, yerr=30, mec="red", marker="x", fmt=' ')
+plt.errorbar(essais[:7], taux_e[:7], yerr=30, mec="black", marker="x", fmt=' ', ecolor="black")
+plt.errorbar(essais[8:], taux_e[8:], yerr=30, mec="black", marker="x", fmt=' ', ecolor="black")
 plt.xlabel("Essais")
 plt.ylabel("Taux d'oxygénation (%)")
-plt.show()
+#plt.show()
 #f.savefig("taux.png", bbox_inches='tight',dpi=600)
 
 # figure pouls
 
 g = plt.figure()
-plt.plot(essais, pouls_c, "bo", label="oximètre commercial")
-plt.plot(essais, pouls_e, "rx", label="résultats expérimentaux")
+plt.plot(essais, pouls_c, "ko", label="oximètre commercial")
+plt.plot(essais[:5], pouls_e[:5], "kx", label="résultats expérimentaux")
+plt.plot(essais[8:], pouls_e[8:], "kx")
 plt.legend()
 plt.xlabel("Essais")
 plt.ylabel("Pouls (bpm)")
